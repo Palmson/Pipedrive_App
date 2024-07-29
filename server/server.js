a73e60946a261b4b905274baa0f0b237668222b9
SliDeR PAlmson <sliderasmail@gmail.com>
2024-07-29 19:31:10 +0300

    some fixes
    switched to html structure

--- server/server.js
+++ server/server.js
@@ -4,9 +4,6 @@ const helmet = require('helmet');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
-
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +20,7 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-  res.render('index', { dealId: null });
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 const submitRouter = require('./routes/submit');
@@ -33,3 +30,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
--- server/utils/pipedrive.js
+++ server/utils/pipedrive.js
@@ -1,54 +1,18 @@
 const axios = require('axios');
 
-const apiToken = process.env.API_TOKEN;
-const companyDomain = process.env.COMPANY_DOMAIN;
+const PIPEDRIVE_API_TOKEN = process.env.PIPEDRIVE_API_TOKEN;
+const PIPEDRIVE_API_BASE_URL = 'https://api.pipedrive.com/v1';
 
-const customFieldKeys = {
-  client: process.env.CLIENT_FIELD,
-  phone: process.env.PHONE_FIELD,
-  email: process.env.EMAIL_FIELD,
-  jobType: process.env.JOBTYPE_FIELD,
-  jobSource: process.env.JOBSOURCE_FIELD,
-  jobDescription: process.env.JOBDESCRIPTION_FIELD,
-  address: process.env.ADDRESS_FIELD,
-  city: process.env.CITY_FIELD,
-  state: process.env.STATE_FIELD,
-  zipCode: process.env.ZIPCODE_FIELD,
-  area: process.env.AREA_FIELD,
-  startDate: process.env.STARTDATE_FIELD,
-  startTime: process.env.STARTTIME_FIELD,
-  endTime: process.env.ENDTIME_FIELD,
-  technician: process.env.TECH_FIELD
-};
-
-const updateDeal = (dealId, formData) => {
-  const apiUrl = `https://${companyDomain}.pipedrive.com/api/v1/deals/${dealId}?api_token=${apiToken}`;
-
-  const dealData = {
-    custom_fields: {
-      [customFieldKeys.client]: `${formData.firstName} ${formData.lastName}`,
-      [customFieldKeys.phone]: formData.phone,
-      [customFieldKeys.email]: formData.email,
-      [customFieldKeys.jobType]: formData.jobType,
-      [customFieldKeys.jobSource]: formData.jobSource,
-      [customFieldKeys.jobDescription]: formData.jobDescription,
-      [customFieldKeys.address]: formData.address,
-      [customFieldKeys.city]: formData.city,
-      [customFieldKeys.state]: formData.state,
-      [customFieldKeys.zipCode]: formData.zipCode,
-      [customFieldKeys.area]: formData.area,
-      [customFieldKeys.startDate]: formData.startDate,
-      [customFieldKeys.startTime]: formData.startTime,
-      [customFieldKeys.endTime]: formData.endTime,
-      [customFieldKeys.technician]: formData.techSelect
-    }
-  };
-
-  return axios.put(apiUrl, dealData, {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-};
+async function updateDeal(dealId, formData) {
+  try {
+    const response = await axios.put(
+      `${PIPEDRIVE_API_BASE_URL}/deals/${dealId}?api_token=${PIPEDRIVE_API_TOKEN}`,
+      formData
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(`Pipedrive API Error: ${error.response ? error.response.data.error : error.message}`);
+  }
+}
 
 module.exports = { updateDeal };
