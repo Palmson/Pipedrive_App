18b5baa28752a50b329a7aa4715461c5f4307306
SliDeR PAlmson <sliderasmail@gmail.com>
2024-07-29 17:11:43 +0300

    updated server.js
    updated path in server.js

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ const helmet = require('helmet');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
